b8af44fca52d2b4c96313e57d6c912229e155556
Fabio Andrei <tifabio@gmail.com>
2016-01-30 14:45:23 +0000

    Operações de CRUD funcionando (com MongoDB)

--- api/model/base.php
+++ api/model/base.php
@@ -1,59 +1,81 @@
 <?php
 
 class Base {
+    protected $conn;
     protected $db;
     protected $table;
     
     public function __construct($table) {
-        if(!file_exists("json")) {
-            mkdir("json");
-        }
-        $this->db = new JsonDB("./json/");
-        
+        $this->conn = new MongoDB\Driver\Manager(getenv('MONGO_URI'));
+        $this->db = getenv('MONGO_DB');
         $this->table = $table;
-        
-        try {
-            $this->db->selectAll($this->table);
-        } catch (Exception $e) {
-            $this->db->createTable($this->table);
-        }
     }
     
     public function getAll() {
-        $data = $this->db->selectAll($this->table);
-        return ($data == null) ? array() : $data;
-    }
-    
-    public function getById($id) {
-        $data = $this->db->select($this->table, "id", $id);
-        return ($data == null) ? array() : $data[0];
-    }
-    
-    public function save($data, $id = 0) {
-        if ($id > 0) {
-            $data["id"] = $id;
-            $this->db->update($this->table, "id", $id, $data);
-        } else {
-            $data["id"] = $this->incrementId();
-            $this->db->insert($this->table, $data);
+        $data = array();
+        
+        $query = new MongoDB\Driver\Query([]);
+        
+        $cursor = $this->conn->executeQuery($this->db.'.'.$this->table, $query);
+        
+        foreach ($cursor as $document) {
+            $doc = (array) $document;
+            unset($doc["_id"]);
+            $doc["id"] = (string) $document->_id;
+            $data[] = $doc;
         }
         
         return $data;
     }
     
-    public function delete($id) {
-        return $this->db->delete($this->table, "id", $id);
+    public function getById($id) {
+        $data = array();
+        
+        $query = new MongoDB\Driver\Query(["_id" => new MongoDB\BSON\ObjectID($id)]);
+        
+        $cursor = $this->conn->executeQuery($this->db.'.'.$this->table, $query);
+        
+        foreach ($cursor as $document) {
+            $doc = (array) $document;
+            unset($doc["_id"]);
+            $doc["id"] = (string) $document->_id;
+            $data = $doc;
+        }
+        
+        return $data;
     }
     
-    private function incrementId() {
-        $data = $this->getAll();
-        if(empty($data)) {
-            return 1;
+    public function save($data, $id = 0) {
+        $bulk = new MongoDB\Driver\BulkWrite();
+        
+        if ($id > 0) {
+            $data["_id"] = new MongoDB\BSON\ObjectID($id);
+            
+            $filter = ["_id" => $data["_id"]];
+            $update = ['$set' => $data];
+            
+            $bulk->update($filter, $update);
         } else {
-            $last = end($data);
-            $nextId = (int)$last["id"] + 1;
-            return $nextId;
+            $data["_id"] = new MongoDB\BSON\ObjectID();
+            $bulk->insert($data);
         }
+        
+        $this->conn->executeBulkWrite($this->db.'.'.$this->table, $bulk);
+        
+        $data["id"] = (string) $data["_id"];
+        unset($data["_id"]);
+        
+        return $data;
+    }
+    
+    public function delete($id) {
+        $bulk = new MongoDB\Driver\BulkWrite();
+        
+        $filter = ["_id" => new MongoDB\BSON\ObjectID($id)];
+        
+        $bulk->delete($filter);
+        
+        $this->conn->executeBulkWrite($this->db.'.'.$this->table, $bulk);
     }
 }
 
